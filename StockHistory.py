2466853b7da824e5d2a92bccf6fe216121a8fa94
Adam Woodman <adamwoodman@gmail.com>
2020-02-15 18:48:55 -0700

    Adding bounds checking to history before final results processing

--- StockHistory.py
+++ StockHistory.py
@@ -4,6 +4,7 @@ import requests
 import json
 from datetime import datetime
 from alpha_vantage.timeseries import TimeSeries
+import time
 
 # Fetch all data
 URL = "https://www.alphavantage.co/query"
@@ -24,6 +25,8 @@ startYear = 0
 validOption = False
 leftOverCash = 0
 symbol = ""
+startIndex = 0
+endIndex = 0
 
 # Header
 print("*** Dollar Cost Averaging Calculator ***\n")
@@ -36,19 +39,39 @@ def readInputFile():
         inFile = open("StockCombinations.csv", "r")
         for line in inFile:
                 newLine = line.split(",")
+                print("inFile: %s,%s,%s,%s" % (newLine[0].strip(), newLine[1].strip(), newLine[2].strip(), newLine[3].strip()))
                 inData.append({"symbol":newLine[0].strip(), "numYears":newLine[1].strip(), "startYear":newLine[2].strip(), "investPerMonth":newLine[3].strip()})
         return inData
 
 def getStockHistory(stockSym = None):
+	stockHistory = {}
+	totalEnt = 0
+	sortedKeys = []
+
+	numRetries = 20
+
 	if (stockSym == None):
 		symbol = input("Enter a stock or ETF symbol: ")
 	else:
 		symbol = stockSym
+
 	PARAMS = {'function':"TIME_SERIES_MONTHLY",'symbol':symbol,'apikey':key}
-	r = requests.get(url = URL, params = PARAMS)
-	stockHistory = r.json()['Monthly Time Series']
-	sortedKeys = sorted(stockHistory)
-	totalEnt = len(stockHistory)
+
+	gotHistory = False
+	retryNum = 0
+
+	while ((not gotHistory) and (retryNum < numRetries)):
+		try:
+			r = requests.get(url = URL, params = PARAMS)
+			stockHistory = r.json()['Monthly Time Series']
+			sortedKeys = sorted(stockHistory)
+			totalEnt = len(stockHistory)
+			gotHistory = True
+		except:
+			time.sleep(5)
+			retryNum = retryNum + 1
+			#print("Error fetching history...")
+
 	return (stockHistory, sortedKeys, totalEnt, symbol)
 
 def purchaseShares(stockHistory, tradeDate, leftOverCash, dollarsPerMonthInvested, sharesPurchasedTotal, totalInvested):
@@ -58,70 +81,97 @@ def purchaseShares(stockHistory, tradeDate, leftOverCash, dollarsPerMonthInveste
         sharesPurchasedTotal = sharesPurchasedTotal + sharesPurchasedDuringMonth
         totalInvested = totalInvested + (sharePrice * sharesPurchasedDuringMonth)
         leftOverCash = amountToInvestDuringMonth % sharePrice
+        #print("Share price: %s, Amount to Invest: %s, Shares Purchased: %s, Left Over: %s" % (sharePrice, amountToInvestDuringMonth, sharesPurchasedDuringMonth, leftOverCash))
         return (sharesPurchasedTotal, totalInvested, leftOverCash)
 
 def printResults(sharesPurchasedTotal, stockHistory, sortedKeys, totalEnt, totalInvested, menuOption, symbol, firstTradeDate, lastTradeDate):
         # Print results
-        totalCurrValue = sharesPurchasedTotal * float(stockHistory[sortedKeys[totalEnt - 1]]['1. open'])
-        netGain = totalCurrValue - totalInvested
-        percentGain = 100 * netGain / totalInvested
-        # Get start/end months from trade dates
-        startMonth = firstTradeDate.split("-")[1]
-        endMonth = lastTradeDate.split("-")[1]
-        if (int(menuOption) == 1):
-                print("\nResults for investing $%s/mo in %s for the past %s years (%s/%s - %s/%s):" % (dollarsPerMonthInvested, symbol, yearsOfHistory, startMonth, startYear, endMonth, (int(startYear) + yearsOfHistory)))
-        elif ((int(menuOption) == 2) or (int(menuOption) == 3)):
-                print("\nResults for investing $%s/mo in %s for %s years, (%s/%s - %s/%s):" % (dollarsPerMonthInvested, symbol, yearsOfHistory, startMonth, startYear, endMonth, (int(startYear) + yearsOfHistory)))
-        print("- Total money invested: $%.2f (%s shares)" % (totalInvested, sharesPurchasedTotal))
-        print("- Current Total Worth: $%.2f" % totalCurrValue)
-        percentSymbol = "%"
-        print("- You have made $%.2f (%.1f%s gain)" % (netGain, percentGain, str(percentSymbol)))
-        #avgAnnualRateOfReturn = (((totalCurrValue / totalInvested) ** (1 / yearsOfHistory)) - 1 ) * 100
-        #avgAnnualRateOfReturn = percentGain / yearsOfHistory
-        #print("- Average annual rate of return: %.1f%s" % (avgAnnualRateOfReturn, str(percentSymbol)))
+        if (totalInvested == 0):
+                print("Error fetching data, not this many years of history for %s" % symbol)
+        else:
+                totalCurrValue = sharesPurchasedTotal * float(stockHistory[lastTradeDate]['1. open'])
+                #print("Date stocks sold: %s" % sortedKeys[totalEnt - 1])
+                #print("Last trade date: %s" % lastTradeDate)
+                netGain = totalCurrValue - totalInvested
+                percentGain = 100 * netGain / totalInvested
+                # Get start/end months from trade dates
+                startMonth = firstTradeDate.split("-")[1]
+                endMonth = lastTradeDate.split("-")[1]
+                if (int(menuOption) == 1):
+                        print("\nResults for investing $%s/mo in %s for the past %s years (%s/%s - %s/%s):" % (dollarsPerMonthInvested, symbol, yearsOfHistory, startMonth, startYear, endMonth, (int(startYear) + yearsOfHistory)))
+                elif ((int(menuOption) == 2) or (int(menuOption) == 3)):
+                        print("\nResults for investing $%s/mo in %s for %s years, (%s/%s - %s/%s):" % (dollarsPerMonthInvested, symbol, yearsOfHistory, startMonth, startYear, endMonth, (int(startYear) + yearsOfHistory)))
+                print("- Total Invested: $%.2f (%s shares)" % (totalInvested, sharesPurchasedTotal))
+                print("- Total Value: $%.2f" % totalCurrValue)
+                percentSymbol = "%"
+                print("- Total Earnings $%.2f (%.1f%s gain)" % (netGain, percentGain, str(percentSymbol)))
+                #avgAnnualRateOfReturn = (((totalCurrValue / totalInvested) ** (1 / yearsOfHistory)) - 1 ) * 100
+                #avgAnnualRateOfReturn = percentGain / yearsOfHistory
+                #print("- Average annual rate of return: %.1f%s" % (avgAnnualRateOfReturn, str(percentSymbol)))
 
 if (int(menuOption) == 1):
 	stockHistory, sortedKeys, totalEnt, symbol = getStockHistory()
 	yearsOfHistory = int(input("Enter years of history: "))
 	monthsOfHistory = yearsOfHistory * 12
-	dollarsPerMonthInvested = int(input("Enter dollars per month invested: $"))
-	startYear = datetime.now().year - yearsOfHistory
-	# Get the last N months of data, ending on the last trade day of last month
-	# If we are not near the end of current month, end at end of last month
-	if (int(datetime.today().day) < 27):
-		endIndex = -2
-	else:
+	# If enough history exists, proceed
+	if (totalEnt >= monthsOfHistory):
+		dollarsPerMonthInvested = int(input("Enter dollars per month invested: $"))
+		startYear = datetime.now().year - yearsOfHistory
+		# Get the last N months of data, ending on the last trade day of last month
+		# If we are not near the end of current month, end at end of last month
+		#if (int(datetime.today().day) < 27):
+		#	endIndex = -2
+		#else:
+		#	endIndex = -1
+		# Accept most recent entry as the last
 		endIndex = -1
-	startIndex = totalEnt - monthsOfHistory - 1
-	tradeDates = sortedKeys[startIndex:endIndex]
-	for tradeDate in tradeDates:
-		sharesPurchasedTotal, totalInvested, leftOverCash = purchaseShares(stockHistory, tradeDate, leftOverCash, dollarsPerMonthInvested, sharesPurchasedTotal, totalInvested)
-	printResults(sharesPurchasedTotal, stockHistory, sortedKeys, totalEnt, totalInvested, menuOption, symbol, sortedKeys[(totalEnt - monthsOfHistory - 1)], sortedKeys[endIndex])
+		startIndex = totalEnt - monthsOfHistory
+		monthsOfHistory = 12 * yearsOfHistory
+		# Last business day in month varies
+		for day in range(25, 32):
+			try:
+				startIndex = sortedKeys.index("%s-01-%s" % (startYear, day))
+			except:
+				"Do nothing, dont error on exception"
+			else:
+				"Found Index"
+				break
 
+		endIndex = startIndex + monthsOfHistory - 1
+		tradeDates = sortedKeys[startIndex:endIndex]
+		for tradeDate in tradeDates:
+			sharesPurchasedTotal, totalInvested, leftOverCash = purchaseShares(stockHistory, tradeDate, leftOverCash, dollarsPerMonthInvested, sharesPurchasedTotal, totalInvested)
+		printResults(sharesPurchasedTotal, stockHistory, sortedKeys, totalEnt, totalInvested, menuOption, symbol, sortedKeys[(totalEnt - monthsOfHistory - 1)], sortedKeys[endIndex])
+	else:
+		print("Not enough history for this stock from API, try a shorter time period")
+		
 elif (int(menuOption) == 2):
 	stockHistory, sortedKeys, totalEnt, symbol = getStockHistory()
 	startYear = input("Enter starting year (format YYYY): ")
 	yearsOfHistory = int(input("Enter duration (years): "))
 	dollarsPerMonthInvested = int(input("Enter dollars per month invested: $ "))
-	monthsOfHistory = 12 * yearsOfHistory - 1
+	monthsOfHistory = 12 * yearsOfHistory
+	totalMonthsOfHistoryRequired = (int(datetime.now().year) - int(startYear)) * 12
+	if (totalEnt >= totalMonthsOfHistoryRequired):
+		# Last business day in month varies
+		for day in range(25, 32):
+			try:
+				startIndex = sortedKeys.index("%s-01-%s" % (startYear, day))
+			except:
+				"Do nothing, dont error on exception"
+			else:
+				"Found Index"
+				break
 
-	# Last business day in month varies
-	for day in range(25, 32):
-		try:
-			startIndex = sortedKeys.index("%s-01-%s" % (startYear, day))
-		except:
-			"Do nothing, dont error on exception"
-		else:
-			"Found Index"
-			break
+		endIndex = startIndex + monthsOfHistory
+		tradeDates = sortedKeys[startIndex:endIndex]
+		#print(tradeDates)
+		for tradeDate in tradeDates:
+			sharesPurchasedTotal, totalInvested, leftOverCash = purchaseShares(stockHistory, tradeDate, leftOverCash, dollarsPerMonthInvested, sharesPurchasedTotal, totalInvested)
 
-	endIndex = startIndex + monthsOfHistory
-	tradeDates = sortedKeys[startIndex:endIndex]
-	print(tradeDates)
-	for tradeDate in tradeDates:
-		sharesPurchasedTotal, totalInvested, leftOverCash = purchaseShares(stockHistory, tradeDate, leftOverCash, dollarsPerMonthInvested, sharesPurchasedTotal, totalInvested)
-
-	printResults(sharesPurchasedTotal, stockHistory, sortedKeys, totalEnt, totalInvested, menuOption, symbol, sortedKeys[startIndex], sortedKeys[endIndex])
+		printResults(sharesPurchasedTotal, stockHistory, sortedKeys, totalEnt, totalInvested, menuOption, symbol, sortedKeys[startIndex], sortedKeys[endIndex])
+	else:
+		print("Not enough history for this stock from API, try a shorter time period")
 
 elif (int(menuOption) == 3):
 	stockCombinations = readInputFile()
@@ -134,22 +184,29 @@ elif (int(menuOption) == 3):
 		yearsOfHistory = int(entry["numYears"])
 		dollarsPerMonthInvested = int(entry["investPerMonth"])
 		monthsOfHistory = 12 * yearsOfHistory
-		# Last business day in month varies
-		for day in range(25, 32):
-			try:
-				startIndex = sortedKeys.index("%s-01-%s" % (startYear, day))
-			except:
-				"Do nothing, dont error on exception"
-			else:
-				"Found Index"
-				break
+		totalMonthsOfHistoryRequired = (datetime.now().year - startYear) * 12
+		if (totalEnt >= totalMonthsOfHistoryRequired):
+			# Last business day in month varies
+			for day in range(25, 32):
+				try:
+					startIndex = sortedKeys.index("%s-01-%s" % (startYear, day))
+				except:
+					"Do nothing, dont error on exception"
+				else:
+					"Found Index"
+					break
 
-		tradeDates = sortedKeys[startIndex:(startIndex + monthsOfHistory)]
+			endIndex = startIndex + monthsOfHistory - 1
+			tradeDates = sortedKeys[startIndex:endIndex]
 
-		for tradeDate in tradeDates:
-			sharesPurchasedTotal, totalInvested, leftOverCash = purchaseShares(stockHistory, tradeDate, leftOverCash, dollarsPerMonthInvested, sharesPurchasedTotal, totalInvested)
+			for tradeDate in tradeDates:
+				sharesPurchasedTotal, totalInvested, leftOverCash = purchaseShares(stockHistory, tradeDate, leftOverCash, dollarsPerMonthInvested, sharesPurchasedTotal, totalInvested)
 
-		printResults(sharesPurchasedTotal, stockHistory, sortedKeys, totalEnt, totalInvested, menuOption, symbol, sortedKeys[(totalEnt - monthsOfHistory - 1)], sortedKeys[-1])
+			printResults(sharesPurchasedTotal, stockHistory, sortedKeys, totalEnt, totalInvested, menuOption, symbol, sortedKeys[(totalEnt - monthsOfHistory - 1)], sortedKeys[-1])
+			# API seems to like some wait time between requests
+			time.sleep(1)
+		else:
+			print("Not enough history for this stock from API, try a shorter time period")
 
 else:
 	print("Invalid option chosen, try again...")
